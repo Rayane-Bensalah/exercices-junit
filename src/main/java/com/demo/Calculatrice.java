d7765d220e8d89d2bbaa7409cea4e61c9f5ed569
jcdominguez <jcd.pro@gmail.com>
2023-11-03 11:38:51 +0100

    Calculatrice soustraire, multiplier

--- src/main/java/com/demo/Calculatrice.java
+++ src/main/java/com/demo/Calculatrice.java
@@ -5,4 +5,12 @@ public class Calculatrice {
     public static int additionner(int a, int b){
         return a + b;
     }
+
+    public static int soustraire(int a, int b){
+        return a - b;
+    }
+
+    public static int multiplier(int a, int b) {
+        return a * b;
+    }
 }
--- src/test/java/com/demo/CalculatriceTest.java
+++ src/test/java/com/demo/CalculatriceTest.java
@@ -11,4 +11,16 @@ public class CalculatriceTest
 
         assertEquals(5, Calculatrice.additionner(2, 3));
     }
+
+    @Test
+    public void testSoustraire(){
+
+        assertEquals(2, Calculatrice.soustraire(5, 3));
+    }
+
+    @Test
+    public void testMultiplier(){
+
+        assertEquals(15, Calculatrice.multiplier(5, 3));
+    }
 }
