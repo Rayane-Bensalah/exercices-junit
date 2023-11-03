c623f4e722b6cfbe9b6ea4c77f89907511cccbd4
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2023-11-03 14:00:29 +0100

    getImpairs

--- src/main/java/com/demo/Calculatrice.java
+++ src/main/java/com/demo/Calculatrice.java
@@ -1,28 +1,29 @@
 package com.demo;
 
+import java.util.ArrayList;
+
 public class Calculatrice {
 
-    public static int additionner(int a, int b){
-        return a + b;
-    }
+  public static int additionner(int a, int b) { return a + b; }
 
-    public static int soustraire(int a, int b){
-        return a - b;
-    }
+  public static int soustraire(int a, int b) { return a - b; }
 
-    public static int multiplier(int a, int b) {
-        return a * b;
-    }
+  public static int multiplier(int a, int b) { return a * b; }
 
-    public static int max(int a, int b){
-        return a > b ? a : b;
-    }
+  public static int max(int a, int b) { return a > b ? a : b; }
 
-    public static double diviser(double a, double b){
-        return a / b;
-    }
+  public static double diviser(double a, double b) { return a / b; }
 
-    public static double diviserInteger(int a, int b){
-        return a / b;
+  public static double diviserInteger(int a, int b) { return a / b; }
+
+  public static ArrayList<Integer> getImpairs(int max) {
+    ArrayList<Integer> res = new ArrayList<>();
+
+    for (int count = 0; count < max; count++) {
+      if (count % 2 == 1) {
+        res.add(count);
+      }
     }
+    return res;
+  }
 }
--- src/test/java/com/demo/CalculatriceTest.java
+++ src/test/java/com/demo/CalculatriceTest.java
@@ -1,53 +1,58 @@
 package com.demo;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+public class CalculatriceTest {
+  @Test
+  public void testAdditioner() {
 
-public class CalculatriceTest
-{
-    @Test
-    public void testAdditioner(){
+    assertEquals(5, Calculatrice.additionner(2, 3));
+  }
 
-        assertEquals(5, Calculatrice.additionner(2, 3));
-    }
+  @Test
+  public void testSoustraire() {
 
-    @Test
-    public void testSoustraire(){
+    assertEquals(2, Calculatrice.soustraire(5, 3));
+  }
 
-        assertEquals(2, Calculatrice.soustraire(5, 3));
-    }
+  @Test
+  public void testMultiplier() {
 
-    @Test
-    public void testMultiplier(){
+    assertEquals(15, Calculatrice.multiplier(5, 3));
+  }
 
-        assertEquals(15, Calculatrice.multiplier(5, 3));
-    }
+  @Test
+  public void testDiviser() {
 
-    @Test
-    public void testDiviser(){
+    assertEquals(3, Calculatrice.diviser(6, 2));
+  }
+  @Test
+  public void testDiviser2() {
 
-        assertEquals(3,Calculatrice.diviser(6,2));
-    }
-    @Test
-    public void testDiviser2(){
+    assertEquals(2.5, Calculatrice.diviser(5, 2));
+  }
 
-        assertEquals(2.5,Calculatrice.diviser(5,2));
-    }
+  @ParameterizedTest
+  @CsvSource({"2,8,8", "20,8,20", "6,6,6", "-2,8,8", "-2,-6,-2"})
+  public void testMax(int a, int b, int resultat) {
+    assertEquals(resultat, Calculatrice.max(a, b));
+  }
 
-    @ParameterizedTest
-    @CsvSource({ "2,8,8", "20,8,20", "6,6,6", "-2,8,8", "-2,-6,-2" })
-    public void testMax(int a, int b, int resultat){
-        assertEquals(resultat, Calculatrice.max(a,b));
-    }
+  @Test
+  public void testDiviserParZero() {
+    assertThrows(ArithmeticException.class,
+                 () -> { Calculatrice.diviserInteger(2, 0); });
+  }
 
-    @Test
-    public void testDiviserParZero() {
-        assertThrows(ArithmeticException.class, () -> {
-            Calculatrice.diviserInteger(2,0);
-        });
-    }
+  @Test
+  public void testArryListImpair() {
+    ArrayList<Integer> integer = Calculatrice.getImpairs(50);
 
+    assertTrue(integer.getFirst() % 2 == 1);
+  }
 }
